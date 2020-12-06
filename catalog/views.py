aae1c93f32c1f24dd198ae4576c1badd15835948
surajit <surajit@poweredbypeople.io>
2020-12-06 22:24:26 +0300

    add views and template

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Product, Category
 
 # Create your views here.
+
+
+class ProductDetail(DetailView):
+    model = Product
+    template_name = "catalog/single_product.html"
+
+
+class CategoryDetail(DetailView):
+    model = Category
+    template_name = "catalog/single_category.html"
