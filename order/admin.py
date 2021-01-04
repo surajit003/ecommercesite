6812ca30ee2b5e937f1112debb45a88d89ad3e8a
surajit <surajit@poweredbypeople.io>
2021-01-04 09:26:30 +0300

    add order admin

--- order/admin.py
+++ order/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+    )
