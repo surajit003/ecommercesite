396636a7e90a7588201a3e0ac9c8e30cc8ef305d
surajit <surajit@poweredbypeople.io>
2021-01-05 14:37:40 +0300

    add user model

---
+++ user/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    stripe_customer_id = models.CharField(max_length=50, blank=True, null=True)
+    one_click_purchasing = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
