6fdb3048e10d2e45974390cbefe99229afa8dafc
surajit <surajit@poweredbypeople.io>
2021-01-14 11:56:39 +0300

    add profile page

--- user/views.py
+++ user/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 def ProfileView(request, profile_id):
     if request.method == "GET":
         print("entered")
-        return render(request, "account/profile.html")
+        return render(request, "account/profile.html", {"profile_id": profile_id})
