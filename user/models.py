8de1937180cafc88545d99ba3d125e234fa5530a
surajit <surajit@poweredbypeople.io>
2021-01-14 10:57:36 +0300

    add company to user model

--- user/models.py
+++ user/models.py
@@ -19,6 +19,9 @@ class UserProfile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     stripe_customer_id = models.CharField(max_length=50, blank=True, null=True)
     one_click_purchasing = models.BooleanField(default=False)
+    company = models.ForeignKey(
+        Company, on_delete=models.CASCADE, null=True, blank=True
+    )
 
     def __str__(self):
         return self.user.username
