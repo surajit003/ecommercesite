96038b3dbdb99a73e34d2f878f28bc4443b64e11
surajit <surajit@poweredbypeople.io>
2021-01-14 10:50:42 +0300

    add company model

--- user/admin.py
+++ user/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
-from .models import UserProfile
+from .models import UserProfile, Company
+
 
 # Register your models here.
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "active",
+    )
+
+
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
     list_display = ("user",)
--- user/models.py
+++ user/models.py
@@ -2,6 +2,19 @@ from django.db import models
 from django.conf import settings
 
 # Create your models here.
+class Company(models.Model):
+    name = models.CharField(max_length=120)
+    slug = models.SlugField(
+        max_length=50,
+        unique=True,
+        help_text="Unique value for company URL, created from name.",
+    )
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+
 class UserProfile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     stripe_customer_id = models.CharField(max_length=50, blank=True, null=True)
